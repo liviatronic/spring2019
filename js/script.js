2cdccfe44405cb936c95b08000b3ecf19e3596a7
Livia Foldes <livia.tronic@gmail.com>
2019-01-28 23:11:11 -0800

    Change layout to flex, implement responsive nav

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-$(document).ready(function () {
+/* RESPONSIVE NAV */
+var dink = 0;
+
+$(document).ready(function ($) {
+
+    $('.icon').click(function () {
+        dink++;
+        $('nav.sidenav').toggleClass('responsive');
+        if (dink % 2 == 1) {
+            $(this).html("Close");
+        } else {
+            $(this).html("Menu");
+        }
+    });
+
 
     /* MAKE ANCHOR LINKS SMOOTHLY SCROLL */
 
-    $('a').click(function () {
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
-    });
+    // $('a').click(function () {
+    //     $('html, body').animate({
+    //         scrollTop: $($(this).attr('href')).offset().top
+    //     }, 500);
+    //     return false;
+    // });
 
     /* CALENDAR TOGGLE */
 
